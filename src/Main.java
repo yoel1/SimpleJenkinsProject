10c6bf6e1c237385806604faa60dc783f0199da3
yoel1 <yoelasihel@yahoo.com>
2025-02-20 14:17:50 -0600

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println("Hello World!");
+            Thread.sleep(2000);
+
+        }
+    }
+}
